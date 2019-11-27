4fa667126e72fa090018767c4fbc861ef2ff8189
João garcia <joaogarciadelimaneto@gmail.com>
2019-11-27 07:33:46 -0300

    views

--- restaurant/food/views.py
+++ restaurant/food/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Restaurant
 from .serializers import RestaurantSerializer
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
